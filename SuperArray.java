9cd4cc81a5f6c7847f6323ff4c907b3a0d93252a
emmachio <emmany2409@gmail.com>
2020-11-04 00:16:50 -0500

    size

---
+++ SuperArray.java
@@ -0,0 +1,13 @@
+public class SuperArray {
+  private String[] data;
+  private int size;
+
+  public SuperArray() {
+    data = new String[10];
+  }
+
+  public int size() {
+    return size;
+  }
+
+}
