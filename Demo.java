437505e7e67a8687c85044924b6e598b4bd32cf1
emmachio <emmany2409@gmail.com>
2020-11-08 17:02:16 -0500

    done and tested zip

--- Demo.java
+++ Demo.java
@@ -26,18 +26,51 @@ public class Demo{
     return newArray;
   }
 
-  public static void main(String[]args){
-      SuperArray words = new SuperArray();
-      //grouped to save vertical space
-      words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
-      words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
-      words.add("una");    words.add("ebi");     words.add("toro");    words.add("hullo");
+  public static SuperArray zip(SuperArray a, SuperArray b) {
+    SuperArray result = new SuperArray();
+    if(a.size() > b.size()) {
+      for(int i = 0; i < a.size(); i++) {
+        if(i < b.size()) {
+          result.add(a.get(i));
+          result.add(b.get(i));
+        }
+        else {
+          result.add(a.get(i));
+        }
+      }
+    }
+    else {
+      for(int j = 0; j < b.size(); j++) {
+        if(j < a.size()) {
+          result.add(a.get(j));
+          result.add(b.get(j));
+        }
+        else {
+          result.add(b.get(j));
+        }
+      }
+    }
+    return result;
+  }
 
+
+  public static void main(String[]args){
+      // SuperArray words = new SuperArray();
+      // //grouped to save vertical space
+      // words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
+      // words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+      // words.add("una");    words.add("ebi");     words.add("toro");    words.add("hullo");
+      //
+      // SuperArray test = new SuperArray();
+      // test.add("kani");    test.add("uni");     test.add("ebi");     test.add("una");
+      // test.add("una");    test.add("ebi");     test.add("kani");    test.add("una");
+      // test.add("una");    test.add("ebi");     test.add("toro");     test.add("hullo");
+      // System.out.println(findOverlap(words, test));
       SuperArray test = new SuperArray();
-      test.add("kani");    test.add("uni");     test.add("ebi");     test.add("una");
-      test.add("una");    test.add("ebi");     test.add("kani");    test.add("una");
-      test.add("una");    test.add("ebi");     test.add("toro");     test.add("hullo");
-      System.out.println(findOverlap(words, test));
+      test.add("a");    test.add("b");    test.add("c");
+      SuperArray numbers = new SuperArray();
+      numbers.add("0");   numbers.add("1");   numbers.add("2");  
+      System.out.println(zip(test, numbers));
     }
 
 }
